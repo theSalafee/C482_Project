33ba663bfb86bb3126519e271809c22073d12d83
Naasir Bush <naasirbush@icloud.com>
2019-07-21 09:53:49 -0700

    Added Support of addPart, deletePart + comments

--- src/Inventory.java
+++ src/Inventory.java
@@ -10,5 +10,66 @@ public class Inventory {
     private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
     private static ObservableList<Part> allParts = FXCollections.observableArrayList();
 
+    // Inventory Class Methods
+
+    ///////////////////
+    // Parts methods //
+    ///////////////////
+
+    public static void addPart(Part part) {
+
+        // Access ObservableList allparts declared above and add a part
+        allParts.add(part);
+    }
+
+    public static void deletePart(Part part) {
+
+        // Access ObservableList allParts declared above and delete a part
+        allParts.remove(part);
+    }
+
+//    public lookupPart(int partID) {
+//
+//        // Access ObservableList allParts declared above and get part by partID
+//        allParts.get(partID);
+//    }
+
+//    public lookupPart(String partName) {
+//
+//        return ObservableList<Part>;
+//    }
+
+//    public void updatePart(int index, Part part) {
+//
+//        return;
+//    }
+
+
+//    public getAllParts() {
+//
+//
+//    }
+
+    //////////////////////
+    // Products Methods //
+    /////////////////////
+
+//    public void addProduct(Product product) {
+//
+//        return;
+//    }
+
+//    public lookupProduct(int productID) {
+//
+//        return Product;
+//    }
+
+//    public lookupProduct(String productName) {
+//
+//        return ObservableList<Product>;
+//    }
+
+
+
 
 }
