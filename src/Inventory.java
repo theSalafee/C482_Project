9b031177fc05b30ea917d6a8ecc3b2d26980ab7d
Naasir Bush <naasirbush@icloud.com>
2019-07-21 09:12:30 -0700

    Added ObservableList<> allParts and allProducts + additional comments.

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,14 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Inventory {
+
+    // Private Members
+    // ObservableList<> =  A list that allows "listeners" to track changes when they occur
+    // FXCollections = A utility class that consists of static methods that are one-to-one copies of java.util.Collections methods
+    // Listeners =  do some work when an event occurs. They are called as "Event Listeners". Events like click, hover
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+    private static ObservableList<Part> allParts = FXCollections.observableArrayList();
+
 
 }
